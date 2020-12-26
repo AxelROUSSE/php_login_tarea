3787edfc85a48f16c15179473ce67a06aefddcd7
AxelROUSSE <AxelROUSSE264@users.noreply.github.com>
2020-12-26 13:38:57 -0500

    php login

---
+++ vista/index.php
@@ -0,0 +1,23 @@
+<?php include 'partials/head.php';?>
+<?php include 'partials/menu.php';?>
+
+<div class="container">
+
+	<div class="starter-template">
+		<br>
+		<br>
+		<br>
+		<div class="jumbotron">
+			<div class="container">
+				<h1>Registro y login de usuarios</h1>
+				<p>Con php</p>
+				<p>
+					<a href="login.php" class="btn btn-primary btn-lg">iniciar sesion</a>
+				</p>
+			</div>
+		</div>
+	</div>
+
+</div><!-- /.container -->
+
+<?php include 'partials/footer.php';?>
